10b9aa9c339f07fa9583b9533ca5c4e8a24daf7e
ice2017cream <ice2017cream@163.com>
2017-11-06 16:03:00 +0800

    update web.xml, struts.xml

--- TPTEW/src/com/mybatis/MybatisTest.java
+++ TPTEW/src/com/mybatis/MybatisTest.java
@@ -8,34 +8,35 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class MybatisTest {
-		private static SqlSessionFactory sqlSessionFactory;  
-	    private static Reader reader;  
-	  
-	    static {
-	      try {  
-	            reader = Resources.getResourceAsReader("config/Configure.xml");  
-	            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);  
-	        } catch (Exception e) {  
-	            e.printStackTrace();  
-	        }  
-	    
-	    }
-	    public static SqlSessionFactory getSession() {  
-	        return sqlSessionFactory;  
-	    }  
-	  
-	    public static void main(String[] args) {  
-	    	
-	        SqlSession session = sqlSessionFactory.openSession();  
-	        try {  
-	            User user = (User) session.selectOne("config.UserMapper.GetUserByID","em10001");  
-	            System.out.println(user.getUser_id());  
-	            System.out.println(user.getUser_name());  
-	            System.out.println(user.getPhone());  
-	            System.out.println(user.getPassword());  
-	        } finally {  
-	            session.close();  
-	        }  
+	private static SqlSessionFactory sqlSessionFactory;
+	private static Reader reader;
+
+	static {
+		try {
+			reader = Resources.getResourceAsReader("config/Configure.xml");
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static SqlSessionFactory getSession() {
+		return sqlSessionFactory;
+	}
+
+	public static void main(String[] args) {
+
+		SqlSession session = sqlSessionFactory.openSession();
+		try {
+			User user = (User) session.selectOne("config.UserMapper.GetUserByID", "em10001");
+			System.out.println(user.getUser_id());
+			System.out.println(user.getUser_name());
+			System.out.println(user.getPhone());
+			System.out.println(user.getPassword());
+		} finally {
+			session.close();
+		}
 	}
 
 }
